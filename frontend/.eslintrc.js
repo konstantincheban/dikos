2c64588e66f39da826b1a5f21b010e3b3cd5f04c
kcheban <kcheban@cisco.com>
2022-05-21 17:43:03 +0300

    dev: turn off few rules in eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-noninteractive-tabindex': 'off'
+    'jsx-a11y/no-noninteractive-tabindex': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off'
   },
 };
