259070fc3c8d29cc2f836b7a0716de4b343fa1c8
kcheban <kcheban@cisco.com>
2024-01-06 21:14:03 +0200

    backend: remove pino logger as app logger && fix DTO for multi-delete endpoint

--- backend/src/transactions/dto/delete-transactions-payload.dto.ts
+++ backend/src/transactions/dto/delete-transactions-payload.dto.ts
@@ -1,3 +1,8 @@
+import { ArrayNotEmpty, IsArray, IsString } from "class-validator";
+
 export class DeleteTransactionsPayloadDTO {
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
   entries: string[];
 }
